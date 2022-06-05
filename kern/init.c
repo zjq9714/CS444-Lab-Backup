0219c335fbc7fef271e34c74378add3d46c36181
Jiaqi Zhang <zhangji6@oregonstate.edu>
2022-06-05 01:00:41 -0700

    Exercise 6

--- kern/init.c
+++ kern/init.c
@@ -54,6 +54,10 @@ i386_init(void)
 #else
 	// Touch all you want.
 	ENV_CREATE(user_primes, ENV_TYPE_USER);
+    ENV_CREATE(user_yield, ENV_TYPE_USER);
+    ENV_CREATE(user_yield, ENV_TYPE_USER);
+    ENV_CREATE(user_yield, ENV_TYPE_USER);
+
 #endif // TEST*
 
 	// Schedule and run the first user environment!
@@ -111,9 +115,8 @@ mp_main(void)
 	//
 	// Your code here:
     lock_kernel();
+    sched_yield();
 
-	// Remove this after you finish Exercise 6
-	for (;;);
 }
 
 /*
--- kern/sched.c
+++ kern/sched.c
@@ -29,6 +29,23 @@ sched_yield(void)
 	// below to halt the cpu.
 
 	// LAB 4: Your code here.
+	int cur = 0;
+	if (curenv) {
+        cur = ENVX(curenv->env_id);
+    } else {
+        cur = 0;
+    }
+
+    for (int i = 0; i < NENV; i++) {
+        int next = (cur + i) % NENV;
+        if (envs[next].env_status == ENV_RUNNABLE) {
+            env_run(envs + next);
+        }
+    }
+
+    if (curenv && curenv->env_status == ENV_RUNNING) {
+        env_run(curenv);
+    }
 
 	// sched_halt never returns
 	sched_halt();
