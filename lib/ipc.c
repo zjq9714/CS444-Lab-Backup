1ddb09d2debfc0c768d185c56788f299c20c646a
Jiaqi Zhang <zhangji6@oregonstate.edu>
2022-06-07 14:02:42 -0700

    part of exe14

--- lib/ipc.c
+++ lib/ipc.c
@@ -23,8 +23,32 @@ int32_t
 ipc_recv(envid_t *from_env_store, void *pg, int *perm_store)
 {
 	// LAB 4: Your code here.
-	panic("ipc_recv not implemented");
-	return 0;
+	// panic("ipc_recv not implemented");
+    int retval = 0;
+
+    if (from_env_store) {
+        *from_env_store = 0;
+    }
+	if (perm_store) {
+        *perm_store = 0;
+    }
+	if (!pg) {
+        pg = (void*) -1;
+    }
+	
+    retval = sys_ipc_recv(pg);
+	if (retval) {
+        return retval;
+    }
+
+	if (from_env_store) {
+		*from_env_store = thisenv->env_ipc_from;
+    }
+
+	if (perm_store) {
+		*perm_store = thisenv->env_ipc_perm;
+    }
+	return thisenv->env_ipc_value;
 }
 
 // Send 'val' (and 'pg' with 'perm', if 'pg' is nonnull) to 'toenv'.
@@ -39,7 +63,19 @@ void
 ipc_send(envid_t to_env, uint32_t val, void *pg, int perm)
 {
 	// LAB 4: Your code here.
-	panic("ipc_send not implemented");
+	int retval = 0;
+	if (!pg) {
+        pg = (void*)-1;
+    }
+	while ((retval = sys_ipc_try_send(to_env, val, pg, perm))) {
+		if (retval == 0) {
+            break;
+        }
+        if (retval != -E_IPC_NOT_RECV) {
+            panic("not E_IPC_NOT_RECV, %e", retval);
+        }
+		sys_yield();
+	}
 }
 
 // Find the first environment of the given type.  We'll use this to
