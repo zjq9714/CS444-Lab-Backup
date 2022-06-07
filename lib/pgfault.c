cf84cba84415a0a45f68959f80ee7bf2eba3f3a3
Jiaqi Zhang <zhangji6@oregonstate.edu>
2022-06-07 13:35:51 -0700

    exe11

--- lib/pgfault.c
+++ lib/pgfault.c
@@ -24,14 +24,14 @@ void (*_pgfault_handler)(struct UTrapframe *utf);
 void
 set_pgfault_handler(void (*handler)(struct UTrapframe *utf))
 {
-	int r;
-
 	if (_pgfault_handler == 0) {
 		// First time through!
 		// LAB 4: Your code here.
-		panic("set_pgfault_handler not implemented");
+		if (sys_page_alloc(0, (void*)(UXSTACKTOP-PGSIZE), PTE_W|PTE_U|PTE_P) < 0)
+			panic("set_pgfault_handler:sys_page_alloc failed");
+        if (sys_env_set_pgfault_upcall(0, _pgfault_upcall) < 0)
+		    panic("set_pgfault_handler:sys_env_set_pgfault_upcall failed");
 	}
-
 	// Save handler pointer for assembly to call.
 	_pgfault_handler = handler;
 }
